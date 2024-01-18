1c0927c36f717b4ad1dccbf9d71220dc2bbe16c7
Haojia Deng <hdeng20@ucsc.edu>
2024-01-17 16:39:17 -0800

    first commit

---
+++ CP-Making-a-Scene-Template-master/src/scene/play.js
@@ -0,0 +1,15 @@
+class Play extends Phaser.Scene{
+    constructor(){
+        super('playScene')
+        console.log('Play:constructor')
+    }
+    init(stats){
+        console.log('Play: init')
+        this.HP = stats.HP,
+        this.EXP = stats.EXP
+    }
+
+    create(){
+        console.log('Play: create')
+    }
+}
